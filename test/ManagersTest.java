9899a065a19633dbafee70bca2fc83da5194dabe
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-08-21 11:20:06 +0300

    ФЗ-6T: Обновлены методы

--- test/ManagersTest.java
+++ test/ManagersTest.java
@@ -2,17 +2,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagersTest {
+
     @Test
     void shouldReturnInitializedTaskManager() {
         TaskManager taskManager = Managers.getDefault();
-
         assertNotNull(taskManager, "Менеджер задач не инициализирован");
+        assertTrue(taskManager instanceof InMemoryTaskManager, "Должен возвращаться InMemoryTaskManager");
     }
 
     @Test
     void shouldReturnInitializedHistoryManager() {
         HistoryManager historyManager = Managers.getDefaultHistory();
-
         assertNotNull(historyManager, "Менеджер истории не инициализирован");
+        assertTrue(historyManager instanceof InMemoryHistoryManager, "Должен возвращаться InMemoryHistoryManager");
     }
 }
