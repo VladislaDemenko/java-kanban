2341558c70a8834d08161aa22c3d886584545cec
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-08-21 11:11:40 +0300

    ФЗ-6: Тест

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+public class Subtask extends Task {
+    private int epicId;
 
-public class Subtask {
+    public Subtask(int id, String title, String description, String status, int epicId) {
+        super(id, title, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    public void setEpicId(int epicId) {
+        this.epicId = epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                ", epicId=" + epicId +
+                '}';
+    }
 }
