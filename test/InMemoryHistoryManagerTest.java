93000f9168d06e456fcc01acff6165a90ddbffed
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-08-21 13:16:38 +0300

    ФЗ-6T: Изменены названия методов на более короткие

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryHistoryManagerTest {
     @Test
-    void shouldAddTasksToHistory() {
+    void addTasksToHistory() {
         HistoryManager historyManager = Managers.getDefaultHistory();
         Task task1 = new Task(1, "Task 1", "Description", "NEW");
         Task task2 = new Task(2, "Task 2", "Description", "NEW");
@@ -14,46 +14,43 @@ class InMemoryHistoryManagerTest {
         List<Task> history = historyManager.getHistory();
 
         assertEquals(2, history.size(), "Неверное количество задач в истории");
-        assertEquals(task1, history.get(0), "Первая задача в истории не совпадает");
-        assertEquals(task2, history.get(1), "Вторая задача в истории не совпадает");
+        assertEquals(task1, history.get(0), "Первая задача не совпадает");
+        assertEquals(task2, history.get(1), "Вторая задача не совпадает");
     }
 
     @Test
-    void shouldRemoveDuplicatesFromHistory() {
+    void removeDuplicates() {
         HistoryManager historyManager = Managers.getDefaultHistory();
         Task task = new Task(1, "Task", "Description", "NEW");
 
-        // Добавляем задачу несколько раз
         historyManager.add(task);
         historyManager.add(task);
         historyManager.add(task);
 
         List<Task> history = historyManager.getHistory();
 
-        assertEquals(1, history.size(), "В истории должны быть только уникальные задачи");
-        assertEquals(task, history.get(0), "Задача в истории не совпадает");
+        assertEquals(1, history.size(), "Должны быть только уникальные задачи");
+        assertEquals(task, history.get(0), "Задача не совпадает");
     }
 
     @Test
-    void shouldRemoveTaskFromHistory() {
+    void shouldRemoveTask() {
         HistoryManager historyManager = Managers.getDefaultHistory();
         Task task1 = new Task(1, "Task 1", "Description", "NEW");
         Task task2 = new Task(2, "Task 2", "Description", "NEW");
 
         historyManager.add(task1);
         historyManager.add(task2);
-
-        // Удаляем первую задачу
         historyManager.remove(1);
 
         List<Task> history = historyManager.getHistory();
 
-        assertEquals(1, history.size(), "Неверное количество задач в истории после удаления");
-        assertEquals(task2, history.get(0), "Оставшаяся задача в истории не совпадает");
+        assertEquals(1, history.size(), "Неверное количество задач после удаления");
+        assertEquals(task2, history.get(0), "Оставшаяся задача не совпадает");
     }
 
     @Test
-    void shouldPreserveTaskDataInHistory() {
+    void preserveTaskData() {
         HistoryManager historyManager = Managers.getDefaultHistory();
         Task task = new Task(1, "Task", "Description", "NEW");
 
@@ -66,7 +63,7 @@ class InMemoryHistoryManagerTest {
     }
 
     @Test
-    void shouldHandleEmptyHistory() {
+    void handleEmptyHistory() {
         HistoryManager historyManager = Managers.getDefaultHistory();
 
         List<Task> history = historyManager.getHistory();
@@ -75,7 +72,7 @@ class InMemoryHistoryManagerTest {
     }
 
     @Test
-    void shouldNotAddNullTask() {
+    void notAddNullTask() {
         HistoryManager historyManager = Managers.getDefaultHistory();
 
         historyManager.add(null);
