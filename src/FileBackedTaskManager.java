085dfbfd2324bd0e9ea4e6213c821a73fd856206
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-09-03 10:29:58 +0300

    ФЗ-7:Добавлен и реализован метод fromString

--- src/FileBackedTaskManager.java
+++ src/FileBackedTaskManager.java
@@ -10,4 +10,33 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
     }
 
 
+
+    private static Task fromString(String value) {
+        String[] parts = value.split(",");
+        if (parts.length < 5) {
+            return null;
+        }
+
+        int id = Integer.parseInt(parts[0]);
+        String type = parts[1];
+        String name = parts[2];
+        String status = parts[3];
+        String description = parts[4];
+
+        switch (type) {
+            case "TASK":
+                return new Task(id, name, description, status);
+            case "EPIC":
+                return new Epic(id, name, description, status);
+            case "SUBTASK":
+                if (parts.length >= 6) {
+                    int epicId = Integer.parseInt(parts[5]);
+                    return new Subtask(id, name, description, status, epicId);
+                }
+                return null;
+            default:
+                return null;
+        }
+    }
+
 }
