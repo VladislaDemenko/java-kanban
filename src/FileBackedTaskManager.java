a89a83f37c76540067a4fb415506147791b925bf
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-09-03 10:14:13 +0300

    ФЗ-7:Добавлен конструктор в FileBackedTaskManager

---
+++ src/FileBackedTaskManager.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class FileBackedTaskManager extends InMemoryTaskManager {
+    private final File file;
+
+    public FileBackedTaskManager(HistoryManager historyManager, File file) {
+        super(historyManager);
+        this.file = file;
+    }
+
+
+}
