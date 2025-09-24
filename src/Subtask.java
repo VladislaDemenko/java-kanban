3ce2fed6bccafba1e780005d29e334e8b72b37b5
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-09-24 12:30:35 +0300

    ФЗ-8: Добавлен конструктор для корректной работы

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,8 +1,17 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Subtask extends Task {
     private int epicId;
 
     public Subtask(int id, String title, String description, String status, int epicId) {
         super(id, title, description, status);
+        this.epicId = epicId;
+    }
+
+    public Subtask(int id, String title, String dedescription, String status, int epicId,
+                   Duration duration, LocalDateTime startTime) {
+        super(id, title, dedescription, status, duration, startTime);
         this.epicId = epicId;
     }
 
@@ -14,13 +23,14 @@ public class Subtask extends Task {
         this.epicId = epicId;
     }
 
-    @Override
     public String toString() {
         return "Subtask{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", description='" + description + '\'' +
                 ", status='" + status + '\'' +
+                ", duration=" + (duration != null ? duration.toMinutes() + "min" : "null") +
+                ", startTime=" + startTime +
                 ", epicId=" + epicId +
                 '}';
     }
